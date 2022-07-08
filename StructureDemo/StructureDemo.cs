371c1ed93ea02bb3b1f3aeabd794fb7f4ff46e74
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:17:36 +0530

    Add project files.

---
+++ StructureDemo/StructureDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StructureDemo
+{
+    struct MyStruct
+    {
+        //int i = 10; - fields of struct can not initialize at declaration
+         int i;
+        public MyStruct(int ia)
+        {
+            i = ia;  
+        }
+        public void Display()
+        {
+            Console.WriteLine("Display method inside structure = "+i);
+        }
+
+        public static void Main()
+        {
+            MyStruct s = new MyStruct();
+            MyStruct s1 = new MyStruct(100 );
+            MyStruct s2;
+            s2.i = 1;
+            s2.Display();
+            s.i = 10;
+            s.Display();
+            s1.Display();
+            Console.ReadLine();
+        }
+    }
+}
